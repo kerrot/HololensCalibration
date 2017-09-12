ede656db8068018725c3c2249a6c4a2b8df9e66e
kerrot <kerrot>
2017-09-12 17:34:11 +0900

    Init

---
+++ Assets/Script/AttachObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class AttachObject : MonoBehaviour {
+
+    [SerializeField] GameObject Target;
+
+	void Start ()
+    {
+        this.LateUpdateAsObservable().Subscribe(_ =>
+        {
+            transform.localPosition = Target.transform.position;
+            transform.localRotation = Target.transform.rotation;
+        });
+	}
+}
